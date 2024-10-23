b89fd099702abb24248efee2f75e44f57f1d026f
Smita-1991 <autismita5@gmail.com>
2024-10-23 15:06:58 -0400

    Created work for Day 1

---
+++ Day1.py
@@ -0,0 +1,20 @@
+print("Hello World!")
+print('''1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.
+2. Knead the dough for 10 minutes.
+3. Add 3g of Salt.
+4. Leave to rise for 2 hours.
+5. Bake at 200 degrees C for 30 minutes.''')
+
+# print("Hello " +input("What is your name") + "!")
+
+# userName=input("what is your name?")
+# length=len(userName)
+# print(length)
+
+glass1 = "milk"
+glass2 = "juice"
+
+Temp=glass1
+glass1=glass2
+glass2=Temp
+print("glass1: "+glass1+"\nglass2: "+glass2)
